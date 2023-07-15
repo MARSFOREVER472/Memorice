08cbd227bd2a46ce3790b21adce11274adc8d259
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-07-15 09:21:07 -0400

    Se crearon hasta el momento métodos de ejecución del juego.

---
+++ HacerParejas/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HacerParejas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblStatus = new Label();
+            lblTimeLeft = new Label();
+            btnReinicio = new Button();
+            Temporizador = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(258, 127);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(131, 25);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "Unir o desfasar";
+            // 
+            // lblTimeLeft
+            // 
+            lblTimeLeft.AutoSize = true;
+            lblTimeLeft.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTimeLeft.Location = new Point(258, 202);
+            lblTimeLeft.Name = "lblTimeLeft";
+            lblTimeLeft.Size = new Size(202, 28);
+            lblTimeLeft.TabIndex = 1;
+            lblTimeLeft.Text = "Tiempo restante: 30";
+            // 
+            // btnReinicio
+            // 
+            btnReinicio.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnReinicio.Location = new Point(258, 12);
+            btnReinicio.Name = "btnReinicio";
+            btnReinicio.Size = new Size(202, 73);
+            btnReinicio.TabIndex = 2;
+            btnReinicio.Text = "Reiniciar";
+            btnReinicio.UseVisualStyleBackColor = true;
+            btnReinicio.Click += RestartGameEvent;
+            // 
+            // Temporizador
+            // 
+            Temporizador.Interval = 1000;
+            Temporizador.Tick += TimeEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(508, 424);
+            Controls.Add(btnReinicio);
+            Controls.Add(lblTimeLeft);
+            Controls.Add(lblStatus);
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "Memorice";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private Label lblTimeLeft;
+        private Button btnReinicio;
+        private System.Windows.Forms.Timer Temporizador;
+    }
+}
---
+++ HacerParejas/Form1.cs
@@ -0,0 +1,41 @@
+namespace HacerParejas
+{
+    public partial class Form1 : Form
+    {
+        // Método de ejecución inicial de la interfaz.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Método de evento del temporizador.
+        private void TimeEvent(object sender, EventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Método de evento de reiniciar el juego.
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+            // EN INSTANTES...
+        }
+
+        // Método para cargar imágenes.
+        private void LoadPictures()
+        {
+            // EN INSTANTES...
+        }
+
+        // Método para reiniciar el juego.
+        private void RestartGame()
+        {
+            // EN INSTANTES...
+        }
+
+        // Método para revisar imágenes.
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+            // EN INSTANTES...
+        }
+    }
+}
